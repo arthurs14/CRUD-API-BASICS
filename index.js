92a6b0b9639a488ec248139a5395bc22cb530af3
arthurs14 <arthurcs426@gmail.com>
2021-07-06 12:49:44 -0700

    created a routes folder to hold specific routes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 
+import usersRoutes from './routes/user.js';
+
 const app = express();
 const PORT = 5000;
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
+app.use('/users', usersRoutes);
+
 app.get('/', (req, res) => {
-  res.send('Hello from get route!');
+  res.send('Hello from Homepage');
 });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
